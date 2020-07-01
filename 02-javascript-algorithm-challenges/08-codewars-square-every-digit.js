bbf7462473ef813badb45139d74d62127e9d41e5
James Vongampai <jaythomasv29@gmail.com>
2020-07-01 14:07:12 -0700

    added new section and algo

--- 02-javascript-algorithm-challenges/08-codewars-square-every-digit.js
+++ 02-javascript-algorithm-challenges/08-codewars-square-every-digit.js
@@ -1,5 +1,16 @@
 const sq = (num)=> {
     let acc = null
-    console.log(num.toString())
+    let stringNum = (num.toString().split(""))
+    let squaredEveryDigitArr = []
+    for(a of stringNum){
+        let result = Math.pow(Number(a), 2)
+        
+        squaredEveryDigitArr.push(result.toString())
+    }
+    return Number(squaredEveryDigitArr.join(""))
 }
+sq(9119) //811181
+
+
+
 sq(9)
