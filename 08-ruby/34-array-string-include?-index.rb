f255b02aa71d9c687c2dafeb9837d636bcce8ce0
James Vongampai <jaythomasv29@gmail.com>
2020-08-27 19:25:09 -0700

    working with include? method in Ruby

--- 08-ruby/34-array-string-include?-index.rb
+++ 08-ruby/34-array-string-include?-index.rb
@@ -1,14 +1,11 @@
 # Index & Include Methods in Ruby
 
 #array/string.index(ele) - evaluates to the index where ele is found, else it is nil
-p 'hello'.index('z')
-[1,2,3,4,5,6,7].index(6)
-
+p "hello".index("z")
+p [1, 1, 2, 3, 4, 5, 6, 7].index(1)
 
 # evaluates to a boolean
-p 'hello'.include?('a' || 'e' || 'i' || 'o' || 'u')
-p [1,2,3,4,5,6,7].include?(6)
-
-
+p "hello".include?("a" || "e" || "i" || "o" || "u")
+p [1, 2, 3, 4, 5, 6, 7].include?(6)
 
 # reverse method '.reverse
